01742f0dd1aadb4d8cef512f18983000dc3d203a
FilipSjovall <filip.sjovall@gmail.com>
2020-11-06 11:06:08 +0100

    uppdaterad goldensection

--- Lab1/goldensection.m
+++ Lab1/goldensection.m
@@ -1,7 +1,25 @@
 function [X,N] = goldensection(F,a,b,tol)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+lam = a + (1-0.618)*(b-a);
+my  = a + 0.618*(b-a);
+
+N = 0;
+while norm(b-a)>tol
+    N = N + 1;
+    if F(lam)>F(my)
+        a      = lam;
+        lam    = my;
+        my  = a + 0.618*(b-a);  
+    else
+        b      = my;
+        my     = lam;
+        lam = a + (1-0.618)*(b-a);
+    end
+end
+X = [a,b,b-a];
+
+
 end
 
